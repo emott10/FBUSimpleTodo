74bcde709cca95e9c3fde3b641f00544528f2e97
emott10 <ericmott10@gmail.com>
2019-05-28 14:24:34 -0700

    Initial commit

---
+++ app/src/main/java/com/mott/eric/fbusimpletodo/MainActivity.java
@@ -0,0 +1,79 @@
+package com.mott.eric.fbusimpletodo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> items;
+    ArrayAdapter<String> itemsAdapter;
+    ListView lvItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        readItems();
+        itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(itemsAdapter);
+        setupListViewListener();
+    }
+
+    public void onAddItem(View v){
+        EditText etTextItem = (EditText) findViewById(R.id.etNewItem);
+        String itemText = etTextItem.getText().toString();
+        itemsAdapter.add(itemText);
+        etTextItem.setText("");
+        writeItems();
+        Toast.makeText(getApplicationContext(), "Item Added to List", Toast.LENGTH_SHORT).show();
+    }
+
+    private void setupListViewListener(){
+        lvItems.setOnItemLongClickListener((new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                itemsAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        }));
+    }
+
+    private File getDataFile(){
+        return new File(getFilesDir(), "todo.txt");
+    }
+
+    private void readItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error reading file", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    private void writeItems(){
+        try {
+            FileUtils.writeLines(getDataFile(), items);
+        } catch (IOException e) {
+            Log.e("MainActivity", "Error writing the file", e);
+        }
+    }
+}
